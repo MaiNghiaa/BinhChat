a16fd5dc8a64f21257c74d3c0d97f1a24fdb9729
Mainghiaa <nghia.mt210179P@sis.hust.edu.vn>
2024-12-22 11:21:29 +0700

    first code

---
+++ Firebase/firebase-config.js
@@ -0,0 +1,11 @@
+// paste ở đây
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAfkOLpG4jgjHDgha4rXWIF4vidgawZgkQ",
+  authDomain: "book-252dd.firebaseapp.com",
+  projectId: "book-252dd",
+  storageBucket: "book-252dd.firebasestorage.app",
+  messagingSenderId: "452936191995",
+  appId: "1:452936191995:web:ea8813f8c4ec87e23d055e",
+  measurementId: "G-TJCV7HTVEX",
+};
