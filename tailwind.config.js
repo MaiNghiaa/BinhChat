5b034a77dcf9a1ab5e9353b0096a3f78bdda13ab
Ezylynn <110110711+Ezylynn@users.noreply.github.com>
2024-12-29 10:17:34 +0700

    add sign in and sign up page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/main.html'
-
+  content: ['./src/main.html', './src/sign-in.html',
+  './src/sign-up.html'
 
 ],
   theme: {
